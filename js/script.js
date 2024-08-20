964239b5027a3a4ed1c5ea344948c62346f99c0e
Joe Mendez-Hughes <joemendez.ai@gmail.com>
2024-08-20 13:31:51 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -35,7 +35,13 @@ function newFunction() {
 			}
 		});
 
-
+		(function () {
+			  const div = document.querySelector('.bg-image'),
+			    src = div.getAttribute('data-bg').split(','), // Exctracts URLs from data-bg to an array
+			    html = document.documentElement; // A reference to <html> element
+			  html.style.setProperty('--bg-desktop', `url(${src[0]})`);
+			  html.style.setProperty('--bg-mobile', `url(${src[1]})`);  
+			}());
 
 
 
