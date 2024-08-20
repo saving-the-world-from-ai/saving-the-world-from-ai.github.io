fb314cc3d00a2fee0ecebd832f941847ab677a4a
Joe Mendez-Hughes <joemendez.ai@gmail.com>
2024-08-20 16:19:11 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -35,16 +35,6 @@ function newFunction() {
 			}
 		});
 
-		(function () {
-			  const div = document.querySelector('.bg-image'),
-			    src = div.getAttribute('data-bg').split(','), // Exctracts URLs from data-bg to an array
-			    html = document.documentElement; // A reference to <html> element
-			  html.style.setProperty('--bg-desktop', `url(${src[0]})`);
-			  html.style.setProperty('--bg-mobile', `url(${src[1]})`);  
-			}());
-
-
-
 		var skillsTopOffset = $(".skillsSection").offset().top;
 		var statsTopOffset = $(".statsSection").offset().top;
 		var countUpFinished = false;
