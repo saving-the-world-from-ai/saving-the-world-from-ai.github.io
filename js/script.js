370581922620f9963e5794419b2981b9a0c9d9a2
Joe Mendez-Hughes <joemendez.ai@gmail.com>
2024-09-02 17:53:22 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,38 +8,6 @@ function newFunction() {
 
     });
 
-    $(document).ready(function () {
-
-        $('.center').slick({
-            centerMode: true,
-            centerPadding: '60px',
-            slidesToShow: 3,
-            responsive: [
-                {
-                    breakpoint: 768,
-                    settings: {
-                        arrows: false,
-                        centerMode: true,
-                        centerPadding: '40px',
-                        slidesToShow: 3
-                    }
-                },
-                {
-                    breakpoint: 480,
-                    settings: {
-                        arrows: false,
-                        centerMode: true,
-                        centerPadding: '40px',
-                        slidesToShow: 1
-                    }
-                }
-            ]
-        });
-
-    });
-
-
-
 
     $(document).ready(function () {
 
